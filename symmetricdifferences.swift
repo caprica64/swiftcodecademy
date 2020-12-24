d4f4515f7b12e509dc2b074812db0a42fb672723
Rodrigo Monteiro <63541433+caprica64@users.noreply.github.com>
2020-12-24 06:10:33 -0300

    symmetricdifferences

---
+++ symmetricdifferences.swift
@@ -0,0 +1,6 @@
+var oscarWinners: Set = ["Heath Ledger", "Rita Moreno", "Audrey Hepburn", "John Legend"]
+var emmyWinners: Set = ["Peter Dinklage", "John Legend", "Audrey Hepburn", "Rita Moreno"]
+
+// Add your code below 🏆
+var difference = oscarWinners.symmetricDifference(emmyWinners)
+print(difference)
