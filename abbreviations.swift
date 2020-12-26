60bf2c8f75fef50ab156f955997b8f6aee58ad5b
Rodrigo Monteiro <63541433+caprica64@users.noreply.github.com>
2020-12-26 09:42:36 -0300

    Update with removals in dictionary

--- abbreviations.swift
+++ abbreviations.swift
@@ -11,3 +11,25 @@ abbreviations ["LOL"] = "Laugh Out Loud"
 abbreviations.updateValue("Be Right Back", forKey: "BRB")
 
 print(abbreviations)
+
+
+
+var rainbowHex = [
+  "red": "#ff0000",
+  "pink": "#ffc0cb",
+  "yellow": "#ffff00",
+  "maroon": "#800000",
+  "green":  "#00ff00",
+  "blue": "#0000ff",
+  "violet": "#ee82ee"
+]
+
+// Write your code below 🌈
+
+rainbowHex ["maroon"] = nil
+
+rainbowHex.removeValue(forKey: "pink")
+
+rainbowHex.removeAll()
+
+print(rainbowHex)
